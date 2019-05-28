0b4ef86cdfe16b6d6efd07f6978f50412807dfa6
vagrant <vagrant@vagrant.vm>
2019-05-28 06:10:03 +0000

    Welcome page

--- guestbook.py
+++ guestbook.py
@@ -1,14 +1,20 @@
-from flask import flask
+from flask import Flask, render_template
  
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')				#default endpoint
 def index():
-	return '<h1>This works !</h1>'
+	return render_template('index.html')
 
+@app.route('/sign')
+def sign():
+	return render_template('sign.html')
 
-
+@app.route('/home', methods=['GET','POST'])
+def home():
+	links = ['http://www.victoria-duncan.com', 'https://lifehacker.com', 'https://aws.com']
+	return render_template('example.html', links=links)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host='0.0.0.0',debug=True)
